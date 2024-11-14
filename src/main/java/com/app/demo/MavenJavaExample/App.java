bb139a3dffd155dc6150e4257aadc9e8128a3638
Pranav <narayanapranav007@gmail.com>
2024-11-14 15:17:06 +0530

    Sample Maven Project

---
+++ src/main/java/com/app/demo/MavenJavaExample/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.MavenJavaExample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Name: Pranav Roll_No: 22BD1A0557" );
+    }
+}
