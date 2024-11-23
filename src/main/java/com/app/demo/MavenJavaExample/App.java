ec10e2444a9c922ef8ab4076b5dea159d5b37802
Vedanth-Pranav <147240075+Vedanth-Pranav@users.noreply.github.com>
2024-11-23 20:39:55 +0530

    Update App.java

--- src/main/java/com/app/demo/MavenJavaExample/App.java
+++ src/main/java/com/app/demo/MavenJavaExample/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println("These documents are making me go crazy");
+        System.out.println("This is the 3rd line");
     }
 }
