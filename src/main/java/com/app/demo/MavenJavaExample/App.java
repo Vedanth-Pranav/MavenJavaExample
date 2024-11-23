749aabe5aa0bdfd331de468697f228aa1f508dee
Pranav <narayanapranav007@gmail.com>
2024-11-23 20:26:04 +0530

    Message

--- src/main/java/com/app/demo/MavenJavaExample/App.java
+++ src/main/java/com/app/demo/MavenJavaExample/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Name: Pranav Roll_No: 22BD1A0557" );
+        System.out.println( "Hello World!" );
     }
 }
